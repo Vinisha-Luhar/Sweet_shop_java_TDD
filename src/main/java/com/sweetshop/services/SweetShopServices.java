0157b6b17618f70e267a546f01abf664770327f7
Vinisha-Luhar <vinishaluhar11@gmail.com>
2025-07-16 19:40:54 +0530

    Added test cases for addSweet

--- src/main/java/com/sweetshop/services/SweetShopServices.java
+++ src/main/java/com/sweetshop/services/SweetShopServices.java
@@ -1,4 +1,18 @@
 package com.sweetshop.services;
 
+import com.sweetshop.model.SweetShopModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SweetShopServices {
+    private Map<Integer, SweetShopModel> sweetStore = new HashMap<>();
+
+    public boolean addSweet(SweetShopModel sweetShopModel) {
+        if (sweetShopModel.getPrice() < 0 || sweetShopModel.getQuantity() < 0 || sweetStore.containsKey(sweetShopModel.getId())) {
+            return false;
+        }
+        sweetStore.put(sweetShopModel.getId(), sweetShopModel);
+        return true;
+    }
 }
--- src/test/java/com/sweetshop/test/SweetShopTest.java
+++ src/test/java/com/sweetshop/test/SweetShopTest.java
@@ -1,4 +1,30 @@
 package com.sweetshop.test;
 
+import com.sweetshop.model.SweetShopModel;
+import com.sweetshop.services.SweetShopServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class SweetShopTest{
+    private SweetShopServices service;
+
+    @BeforeEach
+    void setUp() {
+        service = new SweetShopServices();
+        service.addSweet(new SweetShopModel(1, "Kaju Katli", "Nut-Based", 50, 20));
+        service.addSweet(new SweetShopModel(2, "Gajar Halwa", "Vegetable-Based", 30, 15));
+        service.addSweet(new SweetShopModel(3, "Gulab Jamun", "Milk-Based", 10, 50));
+    }
+
+    @Test
+    void addSweet_ShouldAddValidSweet() {
+        assertTrue(service.addSweet(new SweetShopModel(4, "Ladoo", "Nut-Based", 20, 10)));
+    }
+
+    @Test
+    void addSweet_WhenDuplicateId_ShouldFail() {
+        assertFalse(service.addSweet(new SweetShopModel(1, "New Sweet", "Any", 15, 10)));
+    }
 }
